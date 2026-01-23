945f09396f09c161d33edc948c4b1826b6938a56
meedoomostafa <mezo.225577@gmail.com>
2026-01-23 21:27:15 +0200

    return Id if success adding info for add endpoints

--- Service.Abstraction/IAdministrationService.cs
+++ Service.Abstraction/IAdministrationService.cs
@@ -11,25 +11,25 @@ public interface IAdministrationService
 {
     Task<APIResponse<IEnumerable<DomainsDTO>>> GetDomainsAsync(bool isActive);
     Task<APIResponse<DomainsDTO>> GetDomainAsync(int id);
-    Task<APIResponse> AddDomain(DomainCreateDTO domainsDto);
+    Task<APIResponse<int>> AddDomain(DomainCreateDTO domainsDto);
     Task<APIResponse> UpdateDomain(DomainUpdateDTO domainsDto);
     Task<APIResponse> DeleteDomain(int id);
 
     Task<APIResponse<IEnumerable<TrackDTO>>> GetTracks(int domainId);
     Task<APIResponse<TrackDTO>> GetTrack(int id);
-    Task<APIResponse> AddTrack(TrackCreateDTO trackDto);
+    Task<APIResponse<int>> AddTrack(TrackCreateDTO trackDto);
     Task<APIResponse> UpdateTrack(TrackUpdateDTO trackDto);
     Task<APIResponse> DeleteTrack(int id);
 
     Task<APIResponse<IEnumerable<TrackSectionDTO>>> GetTrackSections(int trackId);
     Task<APIResponse<TrackSectionDTO>> GetTrackSection(int id);
-    Task<APIResponse> AddTrackSection(TrackSectionCreateDTO trackSectionDto);
+    Task<APIResponse<int>> AddTrackSection(TrackSectionCreateDTO trackSectionDto);
     Task<APIResponse> UpdateTrackSection(TrackSectionUpdateDTO trackSectionDto);
     Task<APIResponse> DeleteTrackSection(int id);
 
     Task<APIResponse<IEnumerable<TrackChoiceDTO>>> GetTrackChoices(int trackId);
     Task<APIResponse<TrackChoiceDTO>> GetTrackChoice(int id);
-    Task<APIResponse> AddTrackChoice(TrackChoiceCreateDTO trackChoiceDto);
+    Task<APIResponse<int>> AddTrackChoice(TrackChoiceCreateDTO trackChoiceDto);
     Task<APIResponse> UpdateTrackChoice(TrackChoiceUpdateDTO trackChoiceDto);
     Task<APIResponse> DeleteTrackChoice(int id);
 }
