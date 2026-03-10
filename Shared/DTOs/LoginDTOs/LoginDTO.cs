545856ca4227d3dbf18bdcd733374e77b001bbd8
meedoomostafa <mezo.225577@gmail.com>
2026-03-10 03:17:29 +0200

    Add Required and MaxLength validation to LoginDTO and RegisterDTO

--- Shared/DTOs/LoginDTOs/LoginDTO.cs
+++ Shared/DTOs/LoginDTOs/LoginDTO.cs
@@ -4,8 +4,13 @@ namespace Shared.DTOs.LoginDTOs;
 
 public sealed class LoginDTO
 {
+    [Required]
     [EmailAddress]
+    [MaxLength(256)]
     public string Email { get; set; }
+
+    [Required]
     [DataType(DataType.Password)]
+    [MaxLength(128)]
     public string Password { get; set; }
 }
--- Shared/DTOs/RegisterDTOs/CustomerRegisterDTO.cs
+++ Shared/DTOs/RegisterDTOs/CustomerRegisterDTO.cs
@@ -7,16 +7,33 @@ namespace Shared.DTOs.RegisterDTOs;
 
 public sealed class CustomerRegisterDTO
 {
+    [Required]
+    [MaxLength(100)]
     public string FirstName { get; set; }
+
+    [MaxLength(100)]
     public string? LastName { get; set; }
+
+    [MaxLength(100)]
     public string? UserName { get; set; }
+
+    [Required]
     [EmailAddress]
+    [MaxLength(256)]
     public string Email { get; set; }
+
+    [Required]
     [DataType(DataType.Password)]
+    [MaxLength(128)]
     public string Password { get; set; }
+
+    [Required]
     [DataType(DataType.Password)]
+    [MaxLength(128)]
     [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
     public string PasswordConfirm { get; set; }
+
     [Phone]
+    [MaxLength(20)]
     public string PhoneNumber { get; set; }
 }
